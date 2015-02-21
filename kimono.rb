0d06bb926a310af4ffbeeb5a6ef1deba5d28ef75
Atakikoo <atakikoo@gmail.com>
2015-02-21 12:24:08 +0100

    Revert "cleaned pony and favicon"
    This reverts commit b1e73d2877fc8c1eb9190529c846a0c360b87987.

--- kimono.rb
+++ kimono.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
 
+require 'pony'
+
+
 set :port, 8080
 
 
@@ -43,8 +46,33 @@ get '/bonnes_adresses' do
   erb :bonnes_adresses
 end
 
-get '/contact' do
+get '/contact/?' do
+  @title = 'Contact'
   erb :contact
+end
+
+post '/contact/?' do
+  options = {
+  :to => 'atakikoo@myself.com',
+  :from => params[:email],
+  :subject => params[:name],
+  :body => params[:message],
+  :via => :smtp,
+  :via_options => {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :domain => 'heroku.com',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+    }
+  }
+  
+  Pony.mail(options)
+  
+  redirect '/contact'
+end
 
 
 get '/a_propos' do
